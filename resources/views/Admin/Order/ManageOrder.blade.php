45be2bbd9c72df1ab4e48d9b05cd20d1916667f7
Jerry Janer <janerjerry99@gmail.com>
2022-09-10 09:54:35 +0800

    editing order table

--- resources/views/Admin/Order/ManageOrder.blade.php
+++ resources/views/Admin/Order/ManageOrder.blade.php
@@ -20,91 +20,49 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 
-<!-- for display mesage -->
+        @if(Session::get('sms'))
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+              <strong> {{session::get('sms')}}</strong>
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hiddden="true">&times;</span>
+              </button>
+            </div>
 
-            @if(Session::get('sms'))
-                <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                  <strong> {{session::get('sms')}}</strong>
-                  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hiddden="true">&times;</span>
-                  </button>
-                </div>
+         @elseif(Session::get('delete_msg'))
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+              <strong> {{session::get('delete_msg')}}</strong>
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hiddden="true">&times;</span>
+              </button>
+            </div>
 
-             @elseif(Session::get('delete_msg'))
-                <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                  <strong> {{session::get('delete_msg')}}</strong>
-                  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hiddden="true">&times;</span>
-                  </button>
-                </div>
+        @elseif(Session::get('order_status_msg'))
+            <div class="alert alert-info alert-dismissible fade show" role="alert">
+              <strong> {{session::get('order_status_msg')}}</strong>
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hiddden="true">&times;</span>
+              </button>
+            </div>
 
-            @elseif(Session::get('order_status_msg'))
-                <div class="alert alert-info alert-dismissible fade show" role="alert">
-                  <strong> {{session::get('order_status_msg')}}</strong>
-                  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hiddden="true">&times;</span>
-                  </button>
-                </div>
-
-            @elseif(Session::get('payment_status_msg'))
-                <div class="alert alert-info alert-dismissible fade show" role="alert">
-                  <strong> {{session::get('payment_status_msg')}}</strong>
-                  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hiddden="true">&times;</span>
-                  </button>
-                </div>
+        @elseif(Session::get('payment_status_msg'))
+            <div class="alert alert-info alert-dismissible fade show" role="alert">
+              <strong> {{session::get('payment_status_msg')}}</strong>
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hiddden="true">&times;</span>
+              </button>
+            </div>
 
 
-            @endif
+        @endif
 
+     
           <div class="card my-5">
               <div class="card-header">
-                <h3 class="card-title" id="orderfont"><b>Manage Orders</b></h3>
-               <!--  
-                 <button type="button" class="btn btn-info btn-sm" style="float: right;" data-bs-toggle="modal" data-bs-target="#add" data-bs-whatever="@fat" id="orderfont"> Send Message</button> -->
+                <h3 class="card-title" id="messagefont"><b>Manage Orders</b></h3>
               </div>
-
+          
               <div class="card-body">
-                <table id="example1" class="table table-bordered table-striped">  
-
-                  <!-- add categories modal -->
-                   <!-- <div class="modal fade" id="add" tabindex="-1" aria-labelledby="add" aria-hidden="true">
-                      <div class="modal-dialog">
-                        <div class="modal-content">
-                          <div class="modal-header">
-                            <h5 class="modal-title" id="add" style="margin-left:150px; font-family: poppins;">Send Message</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                          </div>
-                          <div class="modal-body">
-                          <form action="" method="post">
-
-                          <div class="form-group">
-                              <label id="orderfont">Sender:</label>
-                              <input type="text" name="" placeholder="Sender" style="border-right: none; border-left: none; border-top:none; margin-left: 25px; width: 350px; outline: none;" value="ADMIN" id="orderfont">
-                            </div>
-
-                           <div class="form-group">
-                              <label id="orderfont">To:</label>
-                              <input type="text" name="" placeholder="To" style="border-right: none; border-left: none; border-top:none; margin-left: 57px; width: 350px; outline: none;" id="orderfont">
-                            </div>
-
-                            <div class="form-group">
-                              <label id="orderfont">Message:</label>
-                              <input type="text" name="" placeholder="Message" style="border-right: none; border-left: none; border-top:none;margin-left: 10px; width: 350px; outline: none;">
-                            </div>
-                            
-
-
-                            <div class="modal-footer">
-                              <button id="orderfont" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button> 
-                              <button id="orderfont" type="submit" name="btn" class="btn btn-primary">Send</button>
-                            </div>
-
-                          </form>
-                       </div> -->
-                  <!-- end of caategories modal -->
-
-
+                <table id="example1" class="table table-bordered table-striped"> 
                   <thead>
                     <tr>
                       <th id="orderfont" style="text-align: center;">SL</th>
@@ -118,19 +76,49 @@
                     </tr>
                   </thead>
                   <tbody>
+                  @php($i = 1)
+                  @foreach($orders  as $order)
 
-             
-                   @php($i = 1)
-                   @foreach($orders  as $order)
-                    
-              
+                  <!-- modal for order status -->
+                  <div class="modal fade" id="orderstatus{{$order->id}}" tabindex="-1" aria-labelledby="orderstatus{{$order->id}}" aria-hidden="true">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" id="orderstatus{{$order->id}}" style="font-family: poppins;">Update Order Status</h5>
+                        </div>
+                        <div class="modal-body">
+                          <form action="{{route('update_order_status')}}" method="post">
+                               @csrf
+                            <div class="form-group">
+                              <input type="hidden" class="form-control"  name="id" value="{{$order->id}}">
+                              <label id="orderfont">Order Status</label>
+                              <select name="order_status" class="form-select"  required >
+
+                                  <option value="" hidden id="orderfont" > ---Select Order Status---</option>
+                                  <option id="orderfont">On Process</option>
+                                  <option id="orderfont">On Delivery</option>
+                                  <option id="orderfont">Delivered</option>
+                                  <option id="orderfont">Cancelled</option>
+
+                              </select>                        
+                            </div>
+
+                            <div class="modal-footer">
+                              <button id="orderfont" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button> 
+                              <button id="orderfont" type="submit" class="btn btn-primary">Update</button>
+                            </div>
+
+                          </form>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
                   <tr>
-
                     <td id="orderfont">{{$i++}}</td>
                     <td id="orderfont">{{$order->name}} {{$order->middlename}} {{$order -> lastname}}</td>
                     <td id="orderfont">{{$order->order_total}}</td>
-                    <td id="orderfont">
-
+                    <td>
                       @if($order->order_status =='pending')
 
                         <p id="orderfont" style="text-align: center; color: black; background-color: yellow;">
@@ -162,67 +150,24 @@
                         </p>
 
                       @endif
-
-                    
-                      <!-- 
-                       <button type="button" class="btn btn-outline-dark mt-1" data-bs-toggle="modal" data-bs-target="#orderstatus{{$order->id}}" data-bs-whatever="@fat">
-                            <i class="fas fa-edit"  title="click to Change it">  </i>
-                       </button> -->
-
-                       
-
-                    <div class="modal fade" id="orderstatus{{$order->id}}" tabindex="-1" aria-labelledby="orderstatus{{$order->id}}" aria-hidden="true">
-                      <div class="modal-dialog">
-                        <div class="modal-content">
-                          <div class="modal-header">
-                            <h5 class="modal-title" id="orderstatus{{$order->id}}" style="font-family: poppins;">Update Order Status</h5>
-                            <!-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
-                          </div>
-                          <div class="modal-body">
-                            <form action="{{route('update_order_status')}}" method="post">
-                                 @csrf
-                              <div class="form-group">
-                                <input type="hidden" class="form-control"  name="id" value="{{$order->id}}">
-                                <label id="orderfont">Order Status</label>
-                                <select name="order_status" class="form-select"  required >
-
-                                    <option value="" hidden id="orderfont" > ---Select Order Status---</option>
-                                    <option id="orderfont">On Process</option>
-                                    <option id="orderfont">On Delivery</option>
-                                    <option id="orderfont">Delivered</option>
-                                    <option id="orderfont">Cancelled</option>
-
-                                </select>                        
-                              </div>
-
-                               
-                                 <div class="modal-footer">
-                                  <button id="orderfont" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button> 
-                                  <button id="orderfont" type="submit" class="btn btn-primary">Update</button>
-                                </div>
-                            </form>
-                          </div>
-
-
                     </td>
                     <td id="orderfont">{{\Carbon\Carbon::parse($order->created_at)->toFormattedDateString() }}</td>
-                   
-
                     <td id="orderfont">
 
-                         @if($order -> payment_type == 'Cash_on_Delivery') 
+                       @if($order -> payment_type == 'Cash_on_Delivery') 
 
-                              Cash on Delivery (COD)
+                                Cash on Delivery (COD)
 
-                         @elseif($order -> payment_type == 'Cash_on_Pickup') 
+                       @elseif($order -> payment_type == 'Cash_on_Pickup') 
 
-                              Cash on Pickup (COP)
+                            Cash on Pickup (COP)
 
-                        @endif
+                      @endif
 
                     </td>
+
                     <td id="orderfont">
-                     
+
                       @if($order -> payment_status == 'pending')
 
                         <p id="orderfont" style="text-align: center; color: black; background-color: yellow;">
@@ -243,14 +188,11 @@
 
                       @endif
 
+
                     </td>
 
                     <td>
-                      
-
-                      <!--  action -->
-
-                       <div class="btn-group">
+                      <div class="btn-group">
                         <button id="orderfont" type="button" class="btn btn-default btn-sm dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                           More
                         </button>
@@ -258,6 +200,7 @@
 
                           <li><a class="dropdown-item" href="{{route('view_invoice',['id'=>$order->id])}}" id="orderfont"><i class="fas fa-search-plus"  title="View Invoice"> </i> View Invoice</a></li>
 
+                    
                           <li><a class="dropdown-item" href="#" type="button" data-bs-toggle ="modal" data-bs-target="#orderstatus{{$order->id}}" data-bs-whatever="@fat" id="orderfont"> <i class="fas fa-edit"  title="Edit Order Status">  </i> Edit Order Status</a></li> 
 
                           <li><a class="dropdown-item" href="#" type="button" data-bs-toggle ="modal" data-bs-target="#edit{{$order->id}}" data-bs-whatever="@fat" id="orderfont"> <i class="fas fa-edit"  title="Edit Payment Status">  </i> Edit Payment Status</a></li>
@@ -265,17 +208,18 @@
                           <li><a class="dropdown-item" href="{{route('delete_order',['id'=>$order->id])}}" id="orderfont"> <i class="fas fa-trash"  title="click to delete" > </i> Delete Order</a></li>
                         </ul>
                       </div>
-
                     </td>
-                   </tr>
+                  </tr>
 
+                  <!-- modal for payment status -->
 
-                    <div class="modal fade" id="edit{{$order->id}}" tabindex="-1" aria-labelledby="edit{{$order->id}}" aria-hidden="true">
-                      <div class="modal-dialog">
+                  <div class="modal fade" id="edit{{$order->id}}" tabindex="-1" aria-labelledby="edit{{$order->id}}" aria-hidden="true">
+                    <div class="modal-dialog">
                         <div class="modal-content">
                           <div class="modal-header">
-                            <h5 class="modal-title" id="edit{{$order->id}}" style="font-family: poppins;">Update Payment Status</h5>
-                            <!-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
+                            <h5 class="modal-title" id="edit{{$order->id}}" style="font-family: poppins;">
+                              Update Payment Status
+                            </h5>
                           </div>
                           <div class="modal-body">
                             <form action="{{route('order_update')}}" method="post" >
@@ -286,41 +230,29 @@
                                     <input type="hidden" class="form-control"  name="id" value="{{$order->id}}">                    
                                  </div>
 
-                               <label id="orderfont"> Select Payment Status  </label>
+                                <label id="orderfont"> Select Payment Status  </label>
                                 <select name="payment_status" class="form-select" required >
-                                   
+                    
                                     <option value="" hidden id="orderfont"> ---Select Payment Status---</option>
-                                   <!--  <option id="orderfont">Pending</option> -->
                                     <option id="orderfont">Paid</option>
                                     <option id="orderfont">Cancelled</option>
 
-
                                 </select>                       
-                
                                 <div class="modal-footer">
                                   <button id="orderfont" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button> 
                                   <button id="orderfont" type="submit" class="btn btn-primary">Update</button>
                                 </div>
                             </form>
-                        </div>
+                        </div> 
+                      </div>
+                    </div>
+                  </div>
 
-
-                    
-
-                    @endforeach
-
-                 
-
-                 
+                  @endforeach 
                   </tbody>
                 </table>
               </div>
-              <!-- /.card-body -->
-
-
-
-            </div>
-            
+          </div>
 
 
 
